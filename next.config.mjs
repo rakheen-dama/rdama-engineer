6360256436e03d3b1455b1f705b57e6d3f3aed30
Rakheen Dama <rakheen.dama@binarymash.io>
2023-11-08 22:45:37 +0200

    trying to deploy

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,7 @@ const withMDX = nextMDX({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   basePath: '/rdama-engineer',
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
   images: {
